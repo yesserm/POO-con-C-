9baaeb857afdb35f1055c72b3573f4aebd82b506
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-09-22 23:48:18 -0600

    Agrego un ejemplo de variables

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# POO-con-C-
+# POO-con-CSharp
 En este repositorio trabajaré los conceptos de programación orientada a objetos utilizando el lenguaje de programación C#. Iniciando desde la estructura del lenguaje hasta finalizar con POO en detalle. Adicional a esto una pequeña proporción de formularios de Windows.
---
+++ fundamentosDelLenguajeCSharp/1.VariablesConstantes/VariablesConstantes.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace VariablesConstantes
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            //Declarar una variable
+            float area;
+            
+            //inicializar una variable
+            area=5.2F;
+
+            //imprimimos su valor
+            System.Console.WriteLine("El valor de area es " + area);
+        }
+    }
+}
